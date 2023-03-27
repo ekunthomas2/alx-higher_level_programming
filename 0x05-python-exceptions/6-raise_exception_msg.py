74ec115ca8470f2759fb40b314b0434987189449
ekunthomas2 <ekun-tee@yahoo.com>
2023-03-27 11:42:19 -0700

    6. Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Ekun-Thomas Francis <ekun_tee@yahoo.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
