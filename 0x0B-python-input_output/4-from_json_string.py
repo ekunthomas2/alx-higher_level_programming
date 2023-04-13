ccc35338a94d32be6b17b6fd60c5c4b744933c3b
ekunthomas2 <ekun-tee@yahoo.com>
2023-04-13 01:46:59 -0700

    4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 4-from_json_string.py
+# Ekun-Thomas F <ekun_tee@yahoo.com>
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
